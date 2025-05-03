bfebbf09726c92a8bac33fba92c3c9e96262ff95
seif-elden <seifhesham484@yahoo.com>
2025-05-03 14:29:15 +0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/3D-Portfolio",
+  base: "./",
   plugins: [tailwindcss(), react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
 });
